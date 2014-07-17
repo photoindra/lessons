e3edacbe57a8c2eb91f481c4e716166111ccc939
photoindra <ilyaindra@gmail.com>
2014-07-16 20:35:52 -0500

    Trying to get while look working

--- week02/week_02_01.py
+++ week02/week_02_01.py
@@ -1,5 +1,3 @@
-
-
 # Задание 2.1. Напишите функцию input_int(a, b), которая будет работать так же как функция input() без параметров, но только она будет проверять, что введённая пользователем строка действительно является числом в интервале [a;b], и если это не так, то будет просить пользователя повторить попытку ввода.
 
 # Проверить, что введённая строка является числом можно так:
@@ -15,50 +13,28 @@
 #         i += 1
 #     return result
 
-# def anotherFactorial(r):
-#     print ("starting to calculate factorial of " + str(r))
-#     if r==1: result = 1
-#     else: result = r * anotherFactorial(r-1)
-#     print ("Calculated " + str(r) + "!=" + str(result))
-#     return result
 
-# # x = factorial(3)
-# # print ("3! = " + str(x))
+def input_int(a, b):
+    min_value = a
+    max_value = b
+    print ("Enter number between "+ str(min_value)+" and "+str(max_value))
+    x = input()
+    while x.isdigit():
+        print ("It's a number")
+        x = int(x)
+        print ("I converted it to int")
+        return x
+    else:
+        print ("You need to enter a number")
+        
+    while x > max_value or x < min_value:
+        print ("Please try again to enter number between "+ str(min_value)+"and"+str(max_value))
+        x = input()
+        continue
+    else:
+        print ("The number is correct")
+    return x
 
-# y = anotherFactorial(3)
-# print ("3! = " + str(y))
+y = input_int(1,100)
 
-# def input_int(n):
-#     min_value = 1
-#     max_malue = 100
-#     if x.isdigit():
-#         print "It's a number"
-
-def fibofibo(n):
-  if n == 0: return 0
-  if n == 1: return 1
-  return fibo(n - 1) + fibo(n - 2)
-
-import time
-
-def compare_functions(f, g, arg):
-  i = 0
-  t1 = 0
-  t2 = 0
-  while i < 10:
-    last_time = time.clock()
-    f(arg)
-    t1 += time.clock() - last_time
-    last_time = time.clock()
-    g(arg)
-    t2 += time.clock() - last_time
-    i += 1
-  print(t2 / t1)
-
-# допустим fibo1 и fibo2 - это две функции
-# вычисляющие значения чисел Фибоначчи,
-# но разными методами
-compare_functions(fibo, fibo, 50)
-# после этого вызова на экран будет выведено
-# во сколько раз вторая функция работает быстрее
-# или медленнее первой
+print ("printing last answer " +str(y))
